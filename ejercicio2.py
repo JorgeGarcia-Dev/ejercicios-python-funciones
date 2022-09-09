d2f5378336eec4bb69eaddf48c91d9687c8da8b9
JorgeGarcia-Dev <georgegarciacode@outlook.com>
2022-09-09 12:34:17 -0500

    Arreglos en ejercicio 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -3,7 +3,7 @@ Definir una función max_de_tres(), que tome tres números como argumentos y dev
 mayor de ellos. 
 """
 
-def max(num1, num2, num3):
+def max_de_tres(num1, num2, num3):
     if num1 >= num2 and num1 >= num3:
         print("El número mayor es: ", num1)
     elif num2 >= num1 and num2 >= num3:
@@ -15,4 +15,4 @@ num1 = int(input("Ingresa un número: "))
 num2 = int(input("Ingresa un segundo número: "))
 num3 = int(input("Ingresa un tercer número: "))
 
-max(num1, num2, num3)
+max_de_tres(num1, num2, num3)
